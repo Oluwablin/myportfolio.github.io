ccbf13ee11796d20ee97f344aa874108f1dca0c5
oluwablin <oyelekeseun@outlook.com>
2026-04-05 17:06:33 +0100

    updates portfolio

---
+++ js/site-config.js
@@ -0,0 +1,5 @@
+window.PORTFOLIO_CONFIG = {
+  // If your frontend is hosted on GitHub Pages, point this to your deployed Vercel API URL.
+  // Example: 'https://oyelekeseun.vercel.app/api/contact'
+  contactApiUrl: ''
+};
